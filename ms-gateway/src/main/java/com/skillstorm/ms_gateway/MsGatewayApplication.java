d47c90c821058a81f460d4071d7828309333d84a
hhjfdkl <nate.koroman@gmail.com>
2024-08-14 15:28:20 -0400

    Baseline

---
+++ ms-gateway/src/main/java/com/skillstorm/ms_gateway/MsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.ms_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+}
