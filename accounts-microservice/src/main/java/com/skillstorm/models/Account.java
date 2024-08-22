5f264475bbcdee67a37824ae08ca0c8101e1715b
gianni st. clair <gstclair19@gmail.com>
2024-08-22 11:31:51 -0700

    Get accounts methods ready to be accessed by front end

---
+++ accounts-microservice/src/main/java/com/skillstorm/models/Account.java
@@ -0,0 +1,19 @@
+package com.skillstorm.models;
+
+public class Account {
+
+
+	  String accountDisplayName;
+	  String accountId;
+	  String accountAlias;
+	  boolean isActive;
+	  String applicationDisplayName;
+	  String[] roles;
+	  String[] permissionSets;
+	  String communityNickname;
+	  String jobTitle;
+	  String email;
+	  Meta meta;
+	  
+	  
+}
