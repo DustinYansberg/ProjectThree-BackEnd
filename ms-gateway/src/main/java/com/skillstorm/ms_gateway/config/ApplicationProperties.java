dfda307b2e164d0ed411aeda31eff82988c86670
DustinYansberg <88344280+DustinYansberg@users.noreply.github.com>
2024-08-22 13:30:29 -0700

    Delete ms-gateway/src/main/java/com/skillstorm/ms_gateway/config directory
    deleting old security config

--- ms-gateway/src/main/java/com/skillstorm/ms_gateway/config/ApplicationProperties.java
+++
@@ -1,7 +0,0 @@
-//package com.skillstorm.ms_gateway.config;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//
-//@ConfigurationProperties(prefix = "application")
-//public record ApplicationProperties(String clientOriginUrl) {
-//}
