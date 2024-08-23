4dbac2ce0ccfc49ea942de08e0da3f78337a2ea7
DustinYansberg <DustinYansberg@gmail.com>
2024-08-23 10:34:26 -0700

    making cross origin a non-issue. fix later, bb.

--- plugin-bridge/src/main/java/com/document/controllers/DocumentController.java
+++ plugin-bridge/src/main/java/com/document/controllers/DocumentController.java
@@ -1,6 +1,7 @@
 package com.document.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.document.services.DocumentService;
 
 @RestController
 @RequestMapping("/document")
+@CrossOrigin(origins = "*")
 public class DocumentController {
 
     private final DocumentService documentService;
