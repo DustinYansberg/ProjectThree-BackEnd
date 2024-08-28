215174b70f5a03e753f737b804d03b490f90dc50
DustinYansberg <DustinYansberg@gmail.com>
2024-08-28 10:32:08 -0700

    rewriting the plugin on my environment to fix the version issue

--- NotificationPlugin/src/com/skillstorm/models/Notification.java
+++ NotificationPlugin/src/com/skillstorm/models/Notification.java
@@ -3,87 +3,87 @@ package com.skillstorm.models;
 import java.util.Date;
 
 public class Notification {
-	
-	private int notificationId;
-	private String identityId;
-	private String applicationId;
-	private String message;
-	private boolean checked;
-	private Date createdAt;
-	
-	public Notification() {
-	
-	}
-	
-	public Notification(String identityId, String applicationId) {
-		this.identityId = identityId;
-		this.applicationId = applicationId;
-	}
 
-	public Notification(String identityId, String applicationId, String message, boolean checked, Date createdAt) {
-		this.identityId = identityId;
-		this.applicationId = applicationId;
-		this.message = message;
-		this.checked = checked;
-		this.createdAt = createdAt;
-	}
+    private int notificationId;
+    private String identityId;
+    private String applicationId;
+    private String message;
+    private boolean checked;
+    private Date createdAt;
 
-	public Notification(int notificationId, String identityId, String applicationId, String message, boolean checked,
-			Date createdAt) {
-		this.notificationId = notificationId;
-		this.identityId = identityId;
-		this.applicationId = applicationId;
-		this.message = message;
-		this.checked = checked;
-		this.createdAt = createdAt;
-	}
+    public Notification() {
 
-	public int getNotificationId() {
-		return notificationId;
-	}
+    }
 
-	public void setNotificationId(int notificationId) {
-		this.notificationId = notificationId;
-	}
+    public Notification(String identityId, String applicationId) {
+	this.identityId = identityId;
+	this.applicationId = applicationId;
+    }
 
-	public String getIdentityId() {
-		return identityId;
-	}
+    public Notification(String identityId, String applicationId, String message, boolean checked, Date createdAt) {
+	this.identityId = identityId;
+	this.applicationId = applicationId;
+	this.message = message;
+	this.checked = checked;
+	this.createdAt = createdAt;
+    }
 
-	public void setIdentityId(String identityId) {
-		this.identityId = identityId;
-	}
+    public Notification(int notificationId, String identityId, String applicationId, String message, boolean checked,
+	    Date createdAt) {
+	this.notificationId = notificationId;
+	this.identityId = identityId;
+	this.applicationId = applicationId;
+	this.message = message;
+	this.checked = checked;
+	this.createdAt = createdAt;
+    }
 
-	public String getApplicationId() {
-		return applicationId;
-	}
+    public int getNotificationId() {
+	return notificationId;
+    }
 
-	public void setApplicationId(String applicationId) {
-		this.applicationId = applicationId;
-	}
+    public void setNotificationId(int notificationId) {
+	this.notificationId = notificationId;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getIdentityId() {
+	return identityId;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setIdentityId(String identityId) {
+	this.identityId = identityId;
+    }
 
-	public boolean isChecked() {
-		return checked;
-	}
+    public String getApplicationId() {
+	return applicationId;
+    }
 
-	public void setChecked(boolean checked) {
-		this.checked = checked;
-	}
+    public void setApplicationId(String applicationId) {
+	this.applicationId = applicationId;
+    }
 
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+    public String getMessage() {
+	return message;
+    }
 
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-	
+    public void setMessage(String message) {
+	this.message = message;
+    }
+
+    public boolean isChecked() {
+	return checked;
+    }
+
+    public void setChecked(boolean checked) {
+	this.checked = checked;
+    }
+
+    public Date getCreatedAt() {
+	return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+	this.createdAt = createdAt;
+    }
+
 }
--- NotificationPlugin/src/com/skillstorm/rest/NotificationResource.java
+++ NotificationPlugin/src/com/skillstorm/rest/NotificationResource.java
@@ -24,65 +24,55 @@ import sailpoint.tools.GeneralException;
 @Path("NotificationPlugin")
 public class NotificationResource extends BasePluginResource {
 
-	// this is a required override for the BasePluginResource interface
-	@Override
-	public String getPluginName() {
-		return "NotificationPlugin";
-	}
-	
-	private NotificationService service() {
-		return new NotificationService(this);
-	}
-	
-	@GET
-	@Path("test")
-	@AllowAll
-	@Produces(MediaType.APPLICATION_JSON)
-	public String test() throws GeneralException {
-		return "Hello";
-	}
-	
-	@GET
-	@Path("notifications")
-	@AllowAll
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Notification> getAllNotifications() throws GeneralException {
-		return service().getAllNotifications();
-	}
-	
-	@POST
-	@Path("notification")
-	@AllowAll
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Notification createNotification(Map<String, String> body) throws GeneralException {
-		String identityId = body.get("identityId");
-		String applicationId = body.get("applicationId");
-		return service().createNotification(identityId, applicationId);
-	}
-	
-	@GET
-	@Path("notifications/{identityId}")
-	@AllowAll
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Notification> getNotificationById(@PathParam("identityId") String identityId) throws GeneralException {
-		return service().getNotificationsByIdentityId(identityId);
-	}
-	
-	@PUT
-	@Path("/notifications/check/{identityId}")
-	@AllowAll
-	public int readAll(@PathParam("identityId") String identityId) throws GeneralException {
-		return service().readAllNotificationsByIdentityId(identityId);
-	}
+    // this is a required override for the BasePluginResource interface
+    @Override
+    public String getPluginName() {
+	return "NotificationPlugin";
+    }
+
+    private NotificationService service() {
+	return new NotificationService(this);
+    }
+
+    @GET
+    @Path("test")
+    @AllowAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public String test() throws GeneralException {
+	return "Hello";
+    }
+
+    @GET
+    @Path("notifications")
+    @AllowAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Notification> getAllNotifications() throws GeneralException {
+	return service().getAllNotifications();
+    }
+
+    @POST
+    @Path("notification")
+    @AllowAll
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Notification createNotification(Map<String, String> body) throws GeneralException {
+	String identityId = body.get("identityId");
+	String applicationId = body.get("applicationId");
+	return service().createNotification(identityId, applicationId);
+    }
+
+    @GET
+    @Path("notifications/{identityId}")
+    @AllowAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Notification> getNotificationById(@PathParam("identityId") String identityId) throws GeneralException {
+	return service().getNotificationsByIdentityId(identityId);
+    }
+
+    @PUT
+    @Path("/notifications/check/{identityId}")
+    @AllowAll
+    public int readAll(@PathParam("identityId") String identityId) throws GeneralException {
+	return service().readAllNotificationsByIdentityId(identityId);
+    }
 }
-
-
-
-
-
-
-
-
-
-
