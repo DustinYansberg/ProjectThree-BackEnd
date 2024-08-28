1a2647aad8ff45a80509eca22bd6ed719458acac
DustinYansberg <DustinYansberg@gmail.com>
2024-08-28 11:39:42 -0700

    adding eureka support and updating port number to reflect the expected port on k8s

--- ms-accounts/src/main/java/com/skillstorm/AccountsMicroserviceApplication.java
+++ ms-accounts/src/main/java/com/skillstorm/AccountsMicroserviceApplication.java
@@ -2,12 +2,14 @@ package com.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountsMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(AccountsMicroserviceApplication.class, args);
+    }
 
 }
